872cb4f59b6f9b6622d22ed2bb2defc0f90fb0cb
be-chan <70454487+be-chan@users.noreply.github.com>
2023-12-18 22:59:38 +0900

    退会機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  # destroyメソッドを使う前に実行されるメソッド
+  before_destroy :check_all_events_finished 
+
   # この記述はowner_idを外部キーとして、Eventテーブルをcreated_eventsメソッドという名前で参照しに行くためのものらしい。要するにUserモデルのidとEventモデルのowner_idを結合して参照しているということ。この際にhas_manyというのはUserテーブルのidがEventテーブルのレコードを複数持つことになるので、こういった記述になる。1対多の関係。
-  has_many :created_events, class_name: "Event", foreign_key: "owner_id"
-  has_many :tickets
-  
+  has_many :created_events, class_name: "Event", foreign_key: "owner_id", dependent: :nullify
+  has_many :tickets, dependent: :nullify
+  has_many :participating_events, through: :tickets, source: :event
+
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
@@ -14,5 +18,15 @@ class User < ApplicationRecord
       user.name = nickname
       user.image_url = image_url
     end
+  end
+
+  private
+
+  def check_all_events_finished
+    now = Time.zone.now
+    errors[:base] << "公開中の未終了イベントが存在します" if created_events.where(":now < end_at", now: now).exists?
+    errors[:base] << "未終了の参加イベントが存在します" if participating_events.where(":now < end_at", now: now).exists?
+
+    throw(:abort) unless errors.empty?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :events do
     resources :tickets
   end
+  resources :retirements
+  
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/auth/:provider/callback" => "sessions#create"
